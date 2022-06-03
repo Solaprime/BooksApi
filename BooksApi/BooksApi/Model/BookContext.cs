bce6c6cf6e45c89c78abb364350b89adc72eb141
Adebanjo Oluwasola <adebanjo.oluwasola.37@gmail.com>
2022-06-03 14:28:39 +0100

    Added EFcORE pACKage and created the Model

---
+++ BooksApi/BooksApi/Model/BookContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksApi.Model
+{
+    public class BookContext : DbContext
+    {
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- BooksApi/BooksApi/Startup.cs
+++ BooksApi/BooksApi/Startup.cs
@@ -1,3 +1,4 @@
+using BooksApi.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace BooksApi
 {
@@ -25,6 +27,12 @@ namespace BooksApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<BookContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("BookDb"));
+
+            });
+
             services.AddControllers();
         }
 
